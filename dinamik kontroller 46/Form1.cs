01d61ee3d2a71cdacba4b013339b812d2e9f7363
KenanKnd <kenankendirci2006@gmail.com>
2026-05-07 15:35:43 +0300

    ilk yükleme

---
+++ dinamik kontroller 46/Form1.cs
@@ -0,0 +1,62 @@
+
+
+namespace dinamik_kontroller_46
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int k,l,m = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string a = comboBox1.Text;
+            if (a == "Label")
+            {
+              
+                k += 1;
+                label8.Text = k.ToString();
+
+                Label lbl = new Label();
+
+                lbl.Text = "Ahmet Ğ";
+
+                flowLayoutPanel1.Controls.Add(lbl);
+
+            }
+            else if (a == "Checkbox")
+            {
+
+                l += 1;
+                label9.Text = l.ToString();
+
+
+                CheckBox lis = new CheckBox();
+                lis.Text = "Yunus";
+                flowLayoutPanel2.Controls.Add(lis);
+            }
+            else if (a == "Button")
+            {
+
+                m+= 1;
+                label10.Text = m.ToString();
+
+                Button btn = new Button();
+                btn.Text = "Eray";
+                flowLayoutPanel3.Controls.Add(btn);
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
